44ec43ceb06f14a699226e427f62598beeb3dfd2
TheWCKD <potec.tiberiu@gmail.com>
2021-02-22 13:47:59 +0200

    update

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  const AppTheme._();
   static final lightTheme = ThemeData(
     primarySwatch: Colors.blue,
     visualDensity: VisualDensity.adaptivePlatformDensity,
